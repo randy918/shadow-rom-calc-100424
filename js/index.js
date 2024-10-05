3820e122ac656756a027549ca211c06325052d58
Randy Miller <randy918@gmail.com>
2024-10-05 15:27:40 -0500

    calculator 1.0 done and works 10-05-24-1527

--- js/index.js
+++ js/index.js
@@ -5,7 +5,18 @@ const display = document.getElementById("calculator__display");
 //>  ████████████████████████████████████  FUNCTIONS
 
 function appendToDisplay(input) {
-
 display.value += input;
+}
 
+function clearDisplay() {
+display.value = '';
+}
+
+function calculate() {
+try {
+display.value = eval(display.value);
+}
+catch(error) {
+display.value = "Error";
+}
 }
