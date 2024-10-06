5f87e18e4599299ec9d930ba191221bc40357737
Randy Miller <randy918@gmail.com>
2024-10-06 13:41:51 -0500

    calc works with 0 reset  10-06-24-1341

--- js/index.js
+++ js/index.js
@@ -1,20 +1,51 @@
-//>  ████████████████████████████████████  VARIABLES
+//> ████████████████████████████████████  PREAMBLE
+
+"use strict";
+
+//> ████████████████████████████████████  INITIAL VARIABLES
 
 const display = document.getElementById("calculator__display");
+
+display.value = 0;
+
+let isZero = true;
 
 //>  ████████████████████████████████████  FUNCTIONS
 
 function appendToDisplay(input) {
+switch (input) {
+case '+':
+case '/':
+case '-':
+case '*':
+input = " " + input + " ";
+}
+
+if (isZero) {
+
+isZero = false;
+
+display.value = display.value.substring(1);
+
+
+}
+
 display.value += input;
 }
 
 function clearDisplay() {
-display.value = '';
+
+display.value = 0;
+refreshZero = true;
+
 }
 
 function calculate() {
 try {
 display.value = eval(display.value);
+
+isZero = true;
+
 }
 catch(error) {
 display.value = "Error";
